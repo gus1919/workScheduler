45328420e43017a3ac6091b793640939cbf38335
Justin Oeters <justin.oeters@gmail.com>
2021-08-22 16:18:27 -0400

    updated date and time stamp

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const m = moment();
 
-// $("#currentDay").text(today.format(dddd, MMMM Do, YYYY))
+//created variable date to capture time; humanReadable to format it; and clockEl
+// to reference the html
+const clockEl = document.getElementById('currentDay')
+
+//this function will display the date and time and update it each second.
+function updateTime () {
+    const date = moment();
+    const humanReadable = date.format('dddd, MMMM Do, YYYY, hh:mm:ssA')
+    clockEl.textContent = humanReadable;
+}
+setInterval(updateTime, 1000); 
+updateTime();
+
